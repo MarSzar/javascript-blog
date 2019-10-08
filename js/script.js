6f1d59a98bbfe57493f1090fbc492dd23d46e08b
Marta Szargało <marta.szargalo@tlen.pl>
2019-10-08 18:35:53 +0200

    Add function - get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     event.preventDefault();
     console.log('clickedElement:', clickedElement);
       clickedElement.classList.add('active');
 
-    /*  [DONE] remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
 
     for(let activeArticle of activeLinks){
@@ -29,6 +29,8 @@
     }
   
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     
     /* find the correct article using the selector (value of 'href' attribute) */
   
